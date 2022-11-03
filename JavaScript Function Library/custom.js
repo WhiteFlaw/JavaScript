509a0c5b8f9c5d69d60de9d27b1692ab12473fe2
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-11-03 11:07:42 +0800

    custom.js bugFix Error '0' in initNowDate()

--- JavaScript Function Library/custom.js
+++ JavaScript Function Library/custom.js
@@ -58,7 +58,7 @@ export const initNowDate = (format = '-') => {
     const year = time.getFullYear()
     let month = time.getMonth() + 1
     let day = time.getDate()
-    return `${year}${format}${month < 10 ? `'0'${month}` : month}${format}${day < 10 ? `'0'${day}` : day}`
+    return `${year}${format}${month < 10 ? `0${month}` : month}${format}${day < 10 ? `0${day}` : day}`
 }
 
 /* 
